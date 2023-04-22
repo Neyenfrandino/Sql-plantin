2bcf239ed3ba7c2e4dcb7a0c5be82b75a8b7232f
NeyenFrandino <Neyenfrandino1@gmail.com>
2023-04-22 18:19:57 -0500

    ejecutando lo aprendido

--- Prueba.sql
+++ Prueba.sql
@@ -9,6 +9,11 @@ primary key (ID)
 );
 alter table animalees modify column id int auto_increment;
 select * from animalees;
+select * from animalees where estado = 'feliz';
+select * from animalees; 
+update animalees set estado = 'feliz' where id = 2 ;
+select * from animalees;
+select * from animalees where estado ='feliz';
 
 
 
