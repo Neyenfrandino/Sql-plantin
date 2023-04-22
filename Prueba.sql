f0363b5a778d368e1d47f11179b8f03ba4466c1a
NeyenFrandino <Neyenfrandino1@gmail.com>
2023-04-22 18:28:23 -0500

    agragando y modificando estados y tipo

--- Prueba.sql
+++ Prueba.sql
@@ -14,6 +14,14 @@ select * from animalees;
 update animalees set estado = 'feliz' where id = 2 ;
 select * from animalees;
 select * from animalees where estado ='feliz';
+select * from animalees ;
+insert into animalees (tipo, estado) values ('pajariyo', 'piyo');
+select * from animalees;
+update animalees set tipo ='pajarillo' where id= 4;
+select * from animalees;
+update animalees set estado='pillo' where id=4;
+select * from animalees;
+
 
 
 
